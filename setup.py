77a355da218c1caf5abb3b921651a19c50209193
أحمد المحمودي (Ahmed El-Mahmoudy) <aelmahmoudy@sabily.org>
2010-06-15 16:26:11 +0300

    Override build & clean commands in a cleaner manner

--- setup.py
+++ setup.py
@@ -5,14 +5,6 @@ import os, sys
 # to install type: 
 # python setup.py install --root=/
 
-# generate data
-import monajat.sqlGenerator
-
-if 'clean' in sys.argv:
-  try: os.unlink('monajat-data/data.db')
-  except OSError: pass
-else: monajat.sqlGenerator.generate('monajat-data')
-
 # list locales
 locales=map(lambda i: ('share/'+i,[''+i+'/monajat.mo',]),glob('locale/*/LC_MESSAGES'))
 # data files
@@ -21,6 +13,23 @@ data_files=[
   ('/etc/xdg/autostart',['monajat-autostart.desktop']),
 ]
 data_files.extend(locales)
+
+from distutils.command.build import build
+from distutils.command.clean import clean
+
+class my_build(build):
+  def run(self):
+    build.run(self)
+    # generate data
+    import monajat.sqlGenerator
+    if not os.path.isfile('monajat-data/data.db'):
+      monajat.sqlGenerator.generate('monajat-data')
+
+class my_clean(clean):
+  def run(self):
+    clean.run(self)
+    try: os.unlink('monajat-data/data.db')
+    except OSError: pass
 
 # do the install
 setup (name='monajat', version='2.3.0',
@@ -31,7 +40,7 @@ setup (name='monajat', version='2.3.0',
       license='GPLv2',
       packages=['monajat'],
       scripts=['monajat-applet', 'monajat-mod'],
+      cmdclass={'build': my_build, 'clean': my_clean},
       data_files=data_files
 
 )
-
